98930ed8d36a2770810b9e780c283194159c0d35
CC Lee <alex112401@gmail.com>
2021-09-27 08:48:54 +0000

    build model

--- schedule/models.py
+++ schedule/models.py
@@ -3,20 +3,20 @@ from django.db import models
 # Create your models here.
 
 class EventList(models.Model):
-    eventname = models.CharField(max_length=50)
-    eventdate = models.CharField(max_length=50)
-    predtime = models.CharField(max_length=50)
-    emerge = models.CharField(max_length=50)
+    eventname = models.CharField(max_length=50, verbose_name='eventname')
+    eventdate = models.CharField(max_length=50, verbose_name='eventdate')
+    predtime = models.FloatField(max_length=50, verbose_name='predtime')
+    emerge = models.CharField(max_length=50, verbose_name='emerge')
 
     def __str__(self):
-        return self.eventname, self.eventdate, self.predtime, self.emerge
+        return '%s %s %s %s'%(self.eventname, self.eventdate, self.predtime, self.emerge)
 
 class ComEventList(models.Model):
-    eventname = models.CharField(max_length=50)
-    eventdate = models.CharField(max_length=50)
-    predtime = models.CharField(max_length=50)
-    emerge = models.CharField(max_length=50)
-    costtime = models.CharField(max_length=50)
+    eventname = models.CharField(max_length=50, verbose_name='eventname')
+    eventdate = models.CharField(max_length=50, verbose_name='eventdate')
+    predtime = models.FloatField(max_length=50, verbose_name='predtime')
+    emerge = models.CharField(max_length=50, verbose_name='emerge')
+    costtime = models.FloatField(max_length=50, verbose_name='costtime')
 
     def __str__(self):
-        return self.eventname, self.eventdate, self.predtime, self.emerge, self.costtime
+        return '%s %s %s %s %s'%(self.eventname, self.eventdate, self.predtime, self.emerge, self.costtime)
